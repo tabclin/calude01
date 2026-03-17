57d8885756a78ca796f727ca72078434ba71a4de
Thiago Buzatto <tab@tabclin.com>
2026-03-17 09:09:23 -0300

    Convert to Flask web app

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
 def hello_world():
-    print("Hello, World!")
+    return "<h1>Hello, World!</h1>"
 
 
 if __name__ == "__main__":
-    hello_world()
+    app.run()
